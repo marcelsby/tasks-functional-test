c331fa0de5747c1ae325a6663116854688ad4854
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-11-17 16:18:43 -0300

    fix: classe de teste com o sufixo 'Test' para o Maven reconhecer o teste e executá-lo

--- src/test/java/br/ce/wcaquino/tasks/functionaltest/FunctionalTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functionaltest/FunctionalTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class TasksTests {
+public class FunctionalTest {
 
   @Test
   public void deveSalvarTarefaComSucesso() {
