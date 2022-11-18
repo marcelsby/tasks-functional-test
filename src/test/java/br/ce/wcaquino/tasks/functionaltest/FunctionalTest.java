cf372e2d133d2428fd7e77941aba91b6a8379e0d
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-11-18 16:03:45 -0300

    fix: try finally para sempre fechar o browser, mesmo em caso de erro

--- src/test/java/br/ce/wcaquino/tasks/functionaltest/FunctionalTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functionaltest/FunctionalTest.java
@@ -14,66 +14,74 @@ public class FunctionalTest {
   public void deveSalvarTarefaComSucesso() {
     WebDriver driver = acessarAplicacao();
 
-    driver.findElement(By.id("addTodo")).click();
+    try {
+      driver.findElement(By.id("addTodo")).click();
 
-    driver.findElement(By.id("task")).sendKeys("Tarefa salva com sucesso - via Selenium");
-    driver.findElement(By.id("dueDate")).sendKeys("10/10/2999");
+      driver.findElement(By.id("task")).sendKeys("Tarefa salva com sucesso - via Selenium");
+      driver.findElement(By.id("dueDate")).sendKeys("10/10/2999");
 
-    driver.findElement(By.id("saveButton")).click();
+      driver.findElement(By.id("saveButton")).click();
 
-    String message = driver.findElement(By.id("message")).getText();
-    Assert.assertEquals("Success!", message);
-
-    driver.quit();
+      String message = driver.findElement(By.id("message")).getText();
+      Assert.assertEquals("Success!", message);
+    } finally {
+      driver.quit();
+    }
   }
 
   @Test
   public void naoDeveSalvarTarefaSemPrazo() {
     WebDriver driver = acessarAplicacao();
 
-    driver.findElement(By.id("addTodo")).click();
+    try {
+      driver.findElement(By.id("addTodo")).click();
 
-    driver.findElement(By.id("task")).sendKeys("Tarefa sem prazo");
+      driver.findElement(By.id("task")).sendKeys("Tarefa sem prazo");
 
-    driver.findElement(By.id("saveButton")).click();
+      driver.findElement(By.id("saveButton")).click();
 
-    String message = driver.findElement(By.id("message")).getText();
-    Assert.assertEquals("Fill the due date", message);
-
-    driver.quit();
+      String message = driver.findElement(By.id("message")).getText();
+      Assert.assertEquals("Fill the due date", message);
+    } finally {
+      driver.quit();
+    }
   }
 
   @Test
   public void naoDeveSalvarTarefaSemDescricao() {
     WebDriver driver = acessarAplicacao();
 
-    driver.findElement(By.id("addTodo")).click();
+    try {
+      driver.findElement(By.id("addTodo")).click();
 
-    driver.findElement(By.id("dueDate")).sendKeys("10/10/2010");
+      driver.findElement(By.id("dueDate")).sendKeys("10/10/2010");
 
-    driver.findElement(By.id("saveButton")).click();
+      driver.findElement(By.id("saveButton")).click();
 
-    String message = driver.findElement(By.id("message")).getText();
-    Assert.assertEquals("Fill the task description", message);
-
-    driver.quit();
+      String message = driver.findElement(By.id("message")).getText();
+      Assert.assertEquals("Fill the task description", message);
+    } finally {
+      driver.quit();
+    }
   }
 
   @Test
   public void naoDeveSalvarTarefaComDataPassada() {
     WebDriver driver = acessarAplicacao();
 
-    driver.findElement(By.id("addTodo")).click();
+    try {
+      driver.findElement(By.id("addTodo")).click();
 
-    driver.findElement(By.id("task")).sendKeys("Erro com data passada");
-    driver.findElement(By.id("dueDate")).sendKeys("10/10/2010");
+      driver.findElement(By.id("task")).sendKeys("Erro com data passada");
+      driver.findElement(By.id("dueDate")).sendKeys("10/10/2010");
 
-    driver.findElement(By.id("saveButton")).click();
+      driver.findElement(By.id("saveButton")).click();
 
-    String message = driver.findElement(By.id("message")).getText();
-    Assert.assertEquals("Due date must not be in past", message);
-
-    driver.quit();
+      String message = driver.findElement(By.id("message")).getText();
+      Assert.assertEquals("Due date must not be in past", message);
+    } finally {
+      driver.quit();
+    }
   }
 
   private WebDriver acessarAplicacao() {
